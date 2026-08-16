8f40fb652e32a05e0602e71b8f6b3dae01c8e04c
Frank Carvalho <65919656+FrankSCarvalho@users.noreply.github.com>
2026-08-16 15:12:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,12 @@ xp = 0
 print(f"---STATUS---\nVida: {vida}\nMana: {mana}\nAtaque: {ataque}\nDefesa: {defesa}\nOuro: {ouro} \nXP: {xp}")
 
 opcao = input("O que você deseja fazer? \n1 - Explorar \n2 - Ver status \n3 - Sair")
+
+if opcao == "1":
+    print("Você decidiu explorar!")
+elif opcao == "2":
+    print(f"---STATUS---\nVida: {vida}\nMana: {mana}\nAtaque: {ataque}\nDefesa: {defesa}\nOuro: {ouro} \nXP: {xp}")
+elif opcao == "3":
+    print("Voce saiu do Jogo!")
+else:
+    print("Opção invalida!")
