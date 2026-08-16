78713da804c11fc04f392711e899f49c368cfe16
Frank Carvalho <65919656+FrankSCarvalho@users.noreply.github.com>
2026-08-16 15:04:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ print("===================================")
 print("Bem vindo ou mundo...")
 nome = input("Digite o nome do seu Personagem: ")
 print(f"Bem Vindo {nome}!")
+
+vida = 100
+mana = 50
+ataque = 10
+defesa = 5
+ouro = 10
+xp = 0
+
+print(f"---STATUS---\nVida: {vida}\nMana: {mana}\nAtaque: {ataque}\nDefesa: {defesa}\nOuro: {ouro} \nXP: {xp}")
