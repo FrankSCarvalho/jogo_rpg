92603926b5a38bbf47ab315df309cc9381c44b99
Frank Carvalho <65919656+FrankSCarvalho@users.noreply.github.com>
2026-08-16 15:20:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,15 @@ print(f"---STATUS---\nVida: {vida}\nMana: {mana}\nAtaque: {ataque}\nDefesa: {def
 opcao = input("O que você deseja fazer? \n1 - Explorar \n2 - Ver status \n3 - Sair")
 
 if opcao == "1":
-    print("Você decidiu explorar!")
+    print("Você decidiu explorar...")
+    print("Você entra em uma floresta coberta pela névoa.")
+    opcao = input("Você encontra dois caminhos: \n1 - Seguir pela trilha \n2 - Voltar \nEscolha:")
+    if opcao == "1":
+        print("Voce decidiu Seguir pela trilha")
+    elif opcao == "2":
+        print("Vece decidiu voltar")
+    else:
+        print("Opção inválida!")
 elif opcao == "2":
     print(f"---STATUS---\nVida: {vida}\nMana: {mana}\nAtaque: {ataque}\nDefesa: {defesa}\nOuro: {ouro} \nXP: {xp}")
 elif opcao == "3":
