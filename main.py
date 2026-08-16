02ebe800f7a00b4a9e2bcc11a5e9a4fc871f0d59
Frank Carvalho <65919656+FrankSCarvalho@users.noreply.github.com>
2026-08-16 14:53:13 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+
+print("===================================")
+print("BEM VINDO À CRÔNICAS DA NÉVOA")
+print("===================================")
+print("Bem vindo ou mundo...")
+print("escolha o nome do seu personagem")
